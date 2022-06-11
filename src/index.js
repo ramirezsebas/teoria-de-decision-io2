1ee9e06853cfd788ed0ee0f33b3e8a64d6bdcdfd
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-06-11 12:36:53 -0400

    Prueba de la normalizacion de la matriz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo");
+const { normalizeMatrix } = require("./bajo_certidumbre/normalize_matrix.js");
+
+function main() {
+  let matrix = [
+    [1, 1 / 5],
+    [5, 1]
+  ];
+
+  let normalized = normalizeMatrix(matrix);
+  console.log(normalized);
+}
+
+main();
