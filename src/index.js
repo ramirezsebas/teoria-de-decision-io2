9eaf54ea5ce7769d0781ac411beed1294abf828a
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-06-11 18:09:57 -0400

    Agregamos al index el uso del promedio de cada fila

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { normalizeMatrix } = require("./bajo_certidumbre/normalize_matrix.js");
+const { getEachRowAverage } = require("./bajo_certidumbre/get_each_row_average.js")
 
 function main() {
   let matrix = [
@@ -6,8 +7,17 @@ function main() {
     [5, 1]
   ];
 
+  console.log("La Matriz de Comparacion seria: ");
+  console.log(matrix);
+
   let normalized = normalizeMatrix(matrix);
+
+  console.log("La Matriz normalizado queda de la siguiente manera: ");
   console.log(normalized);
+
+  const rowsAverage = getEachRowAverage(normalized);
+  console.log("El promedio de cada fila es: ");
+  console.log(rowsAverage);
 }
 
 main();
